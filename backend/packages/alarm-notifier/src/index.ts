fdef62b2d12e6364f29c78d099a39d1b9101075f
Higor <higorpiresoliveira@gmail.com>
2022-03-12 17:03:58 -0300

    add sse server

--- backend/packages/alarm-notifier/src/index.ts
+++ backend/packages/alarm-notifier/src/index.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 import "dotenv/config";
+import cors from "cors";
+import events from "events";
 import express, { Request, Response } from "express";
 
 const app = express();
+
+app.use(cors());
+
+const eventEmitter = new events.EventEmitter();
 
 app.post("/alarm-notifier/:alarmId", async (req: Request, res: Response) => {
     const { alarmId } = req.params;
@@ -20,10 +26,32 @@ app.post("/alarm-notifier/:alarmId", async (req: Request, res: Response) => {
             `\n\nO alarme ${alarmData.id} da casa ${propertyData.id} que fica no endereço ${propertyData.address} detectou um movimento suspeito!`
         );
 
+        eventEmitter.emit("alarm-detected", alarmData.id);
+
         return res.status(201).send();
     } catch (err) {
         return res.status(500).send({ error: "Unexpected error" });
     }
+});
+
+app.get("/alarm-notifier/:alarmId", async (req: Request, res: Response) => {
+    const { alarmId } = req.params;
+
+    res.set({
+        "Cache-Control": "no-cache",
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+    });
+    res.flushHeaders();
+
+    res.write("retry: 10000\n\n");
+
+    eventEmitter.on("alarm-detected", (eventAlarmId) => {
+        if (alarmId === eventAlarmId) {
+            const data = `data: Um movimento foi detectado na casa às ${new Date().toLocaleTimeString()}!\n\n`;
+            res.write(data);
+        }
+    });
 });
 
 app.listen(process.env.PORT, () =>
